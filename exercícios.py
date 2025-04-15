dffa9a0b66c3fd9e4f25e018d5192986140e4353
Alexandre Cainelli <alecainelli@gmail.com>
2025-04-15 18:18:19 -0300

    Exercício 1

---
+++ exercícios.py
@@ -0,0 +1,10 @@
+#Exercício 1: 
+# quantidade = int(input("Digie a quandidade: "))
+# preco = int(input("Digie a preço: "))
+
+# if quantidade > 0 and preco > 0:
+#     print("Dados Válidos.")
+# else:
+#     print("Dados Inválidos")
+
+#Exercício 2
